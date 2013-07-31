26448858c307905b2f60f9938e7bbeda6a665685
emint <emint@emint-G53SX.(none)>
2013-07-31 18:18:51 -0400

    Adding slf4j android logging

--- game-utils/src/com/et/util/mesh/data/MeshData.java
+++ game-utils/src/com/et/util/mesh/data/MeshData.java
@@ -1,9 +1,12 @@
 package com.et.util.mesh.data;
 
+import java.nio.FloatBuffer;
+
 /**
  * Domain object that captures the basic data of most meshes.
  */
 public class MeshData {
-	
+	FloatBuffer verticies;
+	FloatBuffer indices;
   
 }
