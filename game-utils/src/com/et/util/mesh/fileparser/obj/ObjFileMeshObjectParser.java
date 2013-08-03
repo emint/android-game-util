e89f41b700aacbe5846f9bbbaf7fd87e185a97b4
emint <emint@emint-G53SX.(none)>
2013-08-03 14:54:14 -0400

    Adding files for ObjFile parsing logic

---
+++ game-utils/src/com/et/util/mesh/fileparser/obj/ObjFileMeshObjectParser.java
@@ -0,0 +1,13 @@
+package com.et.util.mesh.fileparser.obj;
+
+import com.et.util.mesh.data.MeshObject;
+import com.et.util.mesh.fileparser.MeshObjectParser;
+
+public class ObjFileMeshObjectParser implements MeshObjectParser<ObjFile> {
+
+  @Override
+  public MeshObject parse(ObjFile file) {
+    return null;
+  }
+
+}
